d63e4317b7aeec9efa475138d377c808524da5c1
robertDurst <rsdurst@colby.edu>
2018-11-26 14:26:40 -0500

    Beginnings of receiving messages

--- server.js
+++ server.js
@@ -1,7 +1,33 @@
+const bodyParser = require('body-parser');
 const express = require('express');
-const app = express();
+const twilio = require('./twilio.js');
+
 const port = 3000 || process.env.PORT;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const app = express();
+app.use(bodyParser.json());   
+app.use(bodyParser.urlencoded({extended: true})); 
+
+app.post('/message', async(req, res) => {
+  const body = req.body;
+  console.log(body);
+  if (body.NumMedia > 0) {
+    // image
+    for (var i = 0; i < body.NumMedia; i++) {
+        const mediaUrl = body[`MediaUrl${i}`];
+        const contentType = body[`MediaContentType${i}`];
+        console.log(mediaUrl);
+        console.log(contentType);
+    }
+    console.log("Received an image");
+    twilio.send_text(body.From, "Received image.");
+  } else {
+    // text
+    console.log("Only a message");
+    twilio.send_text(body.From, "Message received.");
+  }
+
+  res.send("Ok");
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- twilio.js
+++ twilio.js
@@ -1,16 +1,18 @@
-// Download the helper library from https://www.twilio.com/docs/node/install
-// Your Account Sid and Auth Token from twilio.com/console
 const accountSid = process.env.TWILIO_SSID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
-const send_text = phone_number => {
+const send_text = (phone_number, message) => {
   client.messages
     .create({
-       body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+       body: message,
       from: process.env.TWILIO_PHONE_NUMBER,
       to: phone_number
     })
     .then(message => console.log(message.sid))
     .done();
+}
+
+module.exports = {
+  send_text,
 }
